75e8adf59a0fdd7eb3e542316e6a79dd32477f2e
ReeceASharp <reecea.sharp@gmail.com>
2021-08-13 02:59:28 -0600

    Startup script, added linux support, broke everything

--- terminal_setup.sh
+++ terminal_setup.sh
@@ -7,16 +7,51 @@
 SESSION="Distributed"
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-COMMAND_TOP_LEFT="java -cp target/*.jar fileSystem.node.Controller"
-COMMAND_TOP_RIGHT="java -cp target/*.jar fileSystem.node.Client"
-COMMAND_MATRIX="java -cp target/*.jar fileSystem.node.ChunkServer"
 
 # Dimensions of Grid of terminals, and percentage of entire terminal
 MATRIX_WIDTH=3
 MATRIX_HEIGHT=2
 MATRIX_PERCENT=70
 
+# Contents can be edited, and these commands will be ran inside their respective terminal
+top_left() {
+  clear
+  cd "$SCRIPT_DIR"
+  java -cp target/Distributed_File_System-*.jar fileSystem.node.Controller 7000
+}
 
+top_right() {
+  clear
+  sleep 5
+  cd "$SCRIPT_DIR"
+  java -cp target/Distributed_File_System-*.jar fileSystem.node.Client localhost 7000
+}
+
+matrix() {
+  echo 'Waiting 5 seconds for the Controller to start up'
+  sleep 5
+  clear
+  cd "$SCRIPT_DIR"
+  java -cp target/Distributed_File_System-*.jar fileSystem.node.ChunkServer localhost 7000
+}
+
+
+# Converts the contents of a function to a string, which allows it to be sent to a tmux session
+function_contents() {
+  while [ "$1" ];  do
+    type "$1" | sed  -n '/^    /{s/^    //p}' | sed '$s/.*/&;/' ; shift
+  done
+
+}
+
+# Dump the functions above to a string with each command delimited by semicolons to be sent to a tmux
+# Also: Remove ending semicolon so other parameters (like the matrix loop number) can be used as arguments
+COMMAND_TOP_LEFT="$(function_contents top_left)"
+COMMAND_TOP_LEFT=${COMMAND_TOP_LEFT:P:L-1}
+COMMAND_TOP_RIGHT="$(function_contents top_right)"
+COMMAND_TOP_RIGHT=${COMMAND_TOP_RIGHT:P:L-1}
+COMMAND_MATRIX="$(function_contents matrix)"
+COMMAND_MATRIX=${COMMAND_MATRIX:P:L-1}
 
 if [ "$(tmux list-sessions | grep $SESSION)" == "" ]
 then
@@ -52,16 +87,20 @@ then
   done
 
 
-  # Send Commands
+  # COMMAND DEPLOY
   # Top Row
   tmux send-keys -t 0 "$COMMAND_TOP_LEFT" Enter
-
   tmux send-keys -t 1 "$COMMAND_TOP_RIGHT" Enter
 
   tmux select-pane -t 2
+
+  # MATRIX SETUP
+  readarray array_dump < "$SCRIPT_DIR/chunkServers_linux"
+
+  # Matrix
   for (( i = 0; i < MATRIX_HEIGHT*MATRIX_WIDTH; i++ ))
   do
-    tmux send-keys "$COMMAND_MATRIX $(( 6000 + i ))" Enter
+    tmux send-keys "$COMMAND_MATRIX" " ${array_dump[i]}" Enter
     tmux select-pane -t +1
   done
 fi
